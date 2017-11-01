a12892efe10b254c92046d14818db05a803c96fb
MARIA CAMILA DUENAS HERRERA <mcduenash@darkstar.org>
2017-11-01 10:58:55 -0500

    evolucion

--- relajacionEDP/laplace.cpp
+++ relajacionEDP/laplace.cpp
@@ -6,11 +6,13 @@
 const double delta = 0.1;
 const double L = 1.479;
 const int N = int (L/delta)+1;
+const int steps = 100;
 
 typedef std::vector <double> Matrix;
 
 void initial_conditions(Matrix & m);
 void boundary_conditions(Matrix & m);
+void evolve (Matrix & m);
 void print (Matrix & m);
 
 int main (void){
@@ -18,6 +20,11 @@ int main (void){
   Matrix data (N*N);
   initial_conditions(data);
   boundary_conditions(data);
+
+  for (int i = 0; i < steps; ++i){
+    evolve(data);
+  }
+  
   print (data);
 
   return 0;
@@ -56,11 +63,30 @@ void boundary_conditions ( Matrix & m){
   }
 }
 
+void evolve (Matrix & m){
+
+  for (int ii = 0; ii<N; ++ii){
+    for (int jj = 0; jj<N; ++jj){
+      if(ii == 0) continue;
+      if(ii == N-1) continue;
+      if(jj == 0) continue;
+      if(jj == N-1) continue;
+      m[ii*N+jj] = (m[(ii+1)*N + jj] +
+		    m[(ii-1)*N +jj] +
+		    m[ii*N + jj+1] +
+		    m[ii*N + jj-1])/4.0;
+    }
+  }
+
+}
+
 void print (Matrix & m){
   
   for (int ii = 0; ii < N; ++ii){
-      for (int jj = 0; jj< N; ++jj){
-	std::cout << m[ii*N+jj] << "\t";
+      for (int jj = 0; jj < N; ++jj){
+	std::cout << ii*delta << "\t"
+		  << jj*delta << "\t"
+		  << m[ii*N + jj] << "\n";
       }
       std::cout << "\n";
   }
